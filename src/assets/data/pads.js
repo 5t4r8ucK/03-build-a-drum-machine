a4e0e346b366a556998a4f59093e8fda44a533ac
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-22 22:29:36 +0200

    Added the pads module that contains all info for pads.

---
+++ src/assets/data/pads.js
@@ -0,0 +1,76 @@
+const pads = [
+  {
+    color: 'red',
+    id: 'bass-drum',
+    keyCode:  81,
+    letter: 'Q',
+    padIndex: "1",
+    sound: './sounds/linn-bass-drum.wav'
+  },
+  {
+    color: 'orange',
+    id: 'snare',
+    keyCode: 87,
+    letter: 'W',
+    padIndex: "2",
+    sound: './sounds/linn-snare-2.wav'
+  },
+  {
+    color: 'yellow',
+    id: 'hi-hat',
+    keyCode: 69,
+    letter: 'E',
+    padIndex: "3",
+    sound: './sounds/linn-hi-hat-open-2.wav'
+  },
+  {
+    color: 'blue',
+    id: 'crash-cymbal',
+    keyCode: 65,
+    letter: 'A',
+    padIndex: "4",
+    sound: './sounds/linn-crash-cymbal.wav'
+  },
+  {
+    color: 'cyan',
+    id: 'tom-tom-1',
+    keyCode: 83,
+    letter: 'S',
+    padIndex: "5",
+    sound: './sounds/linn-tom-tom-1.wav'
+  },
+  {
+    color: 'green',
+    id: 'clap-1',
+    keyCode: 68,
+    letter: 'D',
+    padIndex: "6",
+    sound: './sounds/linn-clap-1.wav'
+  },
+  {
+    color: 'purple',
+    id: 'tambourine',
+    keyCode: 90,
+    letter: 'Z',
+    padIndex: "7",
+    sound: './sounds/linn-tambourine.wav'
+  },
+  {
+    color: 'magenta',
+    id: 'high-conga',
+    keyCode: 88,
+    letter: 'X',
+    padIndex: "8",
+    sound: './sounds/linn-high-conga.wav'
+  },
+  {
+    color: 'mauve',
+    id: 'cowbell',
+    keyCode: 67,
+    letter: 'C',
+    padIndex: "9",
+    sound: './sounds/linn-cowbell.wav'
+  },
+];
+
+export default pads;
